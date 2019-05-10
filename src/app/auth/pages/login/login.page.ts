4474e0ab8932eeb51056e08527c2f63035cbcd18
Fabiano Melo Moreira <fab_melo60@hotmail.com>
2019-05-10 08:55:37 -0300

    Implementação das validações do formulário do login

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuild: FormBuilder) {}
 
   ngOnInit() {
+    this.createForm();
   }
 
+  private createForm() {
+    this.form = this.formBuild.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get email(): FormControl {
+    return this.form.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.form.get('password') as FormControl;
+  }
+
+  onSubmit(): void {
+    console.log('Form: ', this.form.value);
+  }
 }
