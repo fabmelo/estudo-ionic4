45c135ccfd76f62a6a9cc3f92141d5a391291b20
Fabiano Melo Moreira <fab_melo60@hotmail.com>
2019-05-13 16:18:45 -0300

    Refatoração do template da view de tasks para listagem de tasks e mensagem para quando não houver tasks cadastradas

--- src/app/tasks/pages/tasks-list/tasks-list.page.ts
+++ src/app/tasks/pages/tasks-list/tasks-list.page.ts
@@ -1,3 +1,4 @@
+import { TasksService } from './../../services/tasks.service';
 import { Observable, of } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../../models/tasks.model';
@@ -10,15 +11,9 @@ import { Task } from '../../models/tasks.model';
 export class TasksListPage implements OnInit {
   tasks$: Observable<Task[]>;
 
-  constructor() {}
+  constructor(private tasksService: TasksService) {}
 
   ngOnInit() {
-    this.tasks$ = of([
-      { id: '45df45sd4f5s4df', title: 'Aprender Ionic 1', done: false },
-      { id: '45ds4f5sd4f54df', title: 'Aprender Ionic 2', done: false },
-      { id: '5h5fg4h54fg5h44', title: 'Aprender Ionic 3', done: false },
-      { id: '5rwe4r54we5r4we', title: 'Aprender Ionic 4', done: false },
-      { id: '45k4lj5k4ljk4l5', title: 'Aprender Ionic 5', done: false }
-    ]);
+    this.tasks$ = this.tasksService.getAll();
   }
 }
