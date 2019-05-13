9357e42371bd737cdbe480dcf0439741adf2efad
Fabiano Melo Moreira <fab_melo60@hotmail.com>
2019-05-13 15:45:45 -0300

    Implementação final da classe genérica para realizar os CRUDs no aplicativo

--- src/app/core/classes/firestore.class.ts
+++ src/app/core/classes/firestore.class.ts
@@ -10,7 +10,7 @@ export abstract class Firestore<T extends { id: string }> {
     this.collection = path ? this.db.collection(path, queryFn) : null;
   }
 
-  private setItem(item: T, operation: 'set' | 'update'): Promise<T> {
+  private setItem(item: T, operation: string): Promise<T> {
     return this.collection
       .doc<T>(item.id)
       [operation](item)
@@ -21,8 +21,20 @@ export abstract class Firestore<T extends { id: string }> {
     return this.collection.valueChanges();
   }
 
+  getById(id: string): Observable<T> {
+    return this.collection.doc<T>(id).valueChanges();
+  }
+
   create(item: T): Promise<T> {
     item.id = this.db.createId();
     return this.setItem(item, 'set');
+  }
+
+  update(item: T): Promise<T> {
+    return this.setItem(item, 'update');
+  }
+
+  delete(item: T): Promise<void> {
+    return this.collection.doc<T>(item.id).delete();
   }
 }
