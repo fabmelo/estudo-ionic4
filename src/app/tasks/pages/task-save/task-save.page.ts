7078cf756f672cd426b3af1650fab327c9fcf948
fabmelo <fabmelo@gmail.com>
2019-05-13 20:07:24 -0300

    Implementação das validações do form de tasks save

--- src/app/tasks/pages/task-save/task-save.page.ts
+++ src/app/tasks/pages/task-save/task-save.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-save',
   templateUrl: './task-save.page.html',
-  styleUrls: ['./task-save.page.scss'],
+  styleUrls: ['./task-save.page.scss']
 })
 export class TaskSavePage implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.createForm();
   }
 
+  private createForm() {
+    this.form = this.formBuilder.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      done: [false]
+    });
+  }
+
+  onSubmit(): void {}
 }
