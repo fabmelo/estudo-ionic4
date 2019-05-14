44f7eab781b7d62d58b53d753ac17e2eee741f67
Fabiano Melo Moreira <fab_melo60@hotmail.com>
2019-05-14 09:27:54 -0300

    Criação de indice e ordenação de dados na lista

--- src/app/tasks/services/tasks.service.ts
+++ src/app/tasks/services/tasks.service.ts
@@ -3,6 +3,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { Firestore } from '../../core/classes/firestore.class';
 import { Task } from '../models/tasks.model';
 import { AuthService } from '../../core/services/auth.service';
+import { firestore } from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,9 @@ export class TasksService extends Firestore<Task> {
   private init(): void {
     this.authService.authState$.subscribe(user => {
       if (user) {
-        this.setCollection(`/users/${user.uid}/tasks`);
+        this.setCollection(`/users/${user.uid}/tasks`, (ref: firestore.CollectionReference) => {
+          return ref.orderBy('done', 'asc').orderBy('title', 'asc');
+        });
         return;
       }
       this.setCollection(null);
